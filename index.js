635e60b8acbce9732308806c9478e44da0954bf2
Mujtaba Ali <mujiroy01@gmail.com>
2022-08-09 11:39:33 +0500

    Created base api

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+// import routes
+// const posts = require("./routes/post");
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+//Routes
+// app.use("/api/posts", posts);
+
+// Base API
+
+app.get("/", (req, res) => {
+  res.send("Hello I am working");
+});
+app.get("/api", (req, res) => {
+  res.send("API is on fire⚡");
+});
+
+
+
+
+const PORT = process.env.PORT || 5000;
+CONNECTION_URL=process.env.CONNECTION_URL;
+
+
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
